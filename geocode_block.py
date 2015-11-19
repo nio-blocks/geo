330aa56d457ebd6685ee84c89b6af235a888d329
Chris Brackert <cbrackert@gmail.com>
2015-11-19 08:53:28 -0700

    Refactor by creating GeocodeBase - fix #2
    Refactor to use GeocodeBase for Geocode and ReverseGeocode blocks.

--- geocode_block.py
+++ geocode_block.py
@@ -1,63 +1,21 @@
-from enum import Enum
-from geopy.geocoders import Nominatim
-from nio.metadata.properties import ExpressionProperty, StringProperty
-from nio.common.block.base import Block
+from nio.metadata.properties import ExpressionProperty
 from nio.common.discovery import Discoverable, DiscoverableType
-from nio.util.attribute_dict import AttributeDict
+from .geocode_base import GeocodeBase
 
 
 @Discoverable(DiscoverableType.block)
-class Geocode(Block):
+class Geocode(GeocodeBase):
 
     """ Geolocate a query to an address and coordinates """
 
     query = ExpressionProperty(title='Query', default='175 5th Avenue NYC',
                                attr_default=AttributeError)
-    output_prop = StringProperty(
-        title='Output Attribute', default='location', visible=False)
 
-    def __init__(self):
-        super().__init__()
-        self._geolocator = None
-
-    def configure(self, context):
-        super().configure(context)
-        self._geolocator = Nominatim()
-
-    def process_signals(self, signals, input_id='default'):
-        for signal in signals:
-            self._add_location_to_signal(signal)
-        self.notify_signals(signals)
-
-    def _add_location_to_signal(self, signal):
-        """ Adds location data to the signal """
+    def _get_query_from_signal(self, signal):
         try:
-            query = self.query(signal)
+            return self.query(signal)
         except:
             self._logger.exception("Unable to evaluate query")
-            setattr(signal, self.output_prop, None)
-            return
-        try:
-            self._logger.debug(
-                "Geocode query \"{}\"".format(query))
-            location = self._geolocator.geocode(query)
-            self._logger.debug(
-                "Geocode result for query \"{}\": {}".format(query, location))
-            if not location:
-                self._logger.warning(
-                    "No geocode loaction for query: {}".format(query))
-                return
-            location_dict = AttributeDict({
-                'address': location.address,
-                'altitude': location.altitude,
-                'latitude': location.latitude,
-                'longitude': location.longitude,
-                'raw': location.raw
-            })
-            setattr(signal, self.output_prop, location_dict)
-        except:
-            self._logger.exception(
-                "Unable to geocode location from query: {}".format(query))
-        finally:
-            if not getattr(signal, self.output_prop, None):
-                setattr(signal, self.output_prop, None)
+
+    def _get_location(self, query):
+        return self._geolocator.geocode(query)
--- reverse_geocode_block.py
+++ reverse_geocode_block.py
@@ -1,10 +1,7 @@
-from enum import Enum
-from geopy.geocoders import Nominatim
-from nio.metadata.properties import ExpressionProperty, StringProperty, \
+from nio.metadata.properties import ExpressionProperty, \
     ObjectProperty, PropertyHolder
-from nio.common.block.base import Block
 from nio.common.discovery import Discoverable, DiscoverableType
-from nio.util.attribute_dict import AttributeDict
+from .geocode_base import GeocodeBase
 
 
 class GeoPoint(PropertyHolder):
@@ -15,58 +12,19 @@ class GeoPoint(PropertyHolder):
 
 
 @Discoverable(DiscoverableType.block)
-class ReverseGeocode(Block):
+class ReverseGeocode(GeocodeBase):
 
     """ Find the address corresponding to a set of coordinates """
 
     location = ObjectProperty(GeoPoint, title='Query Location')
-    output_prop = StringProperty(
-        title='Output Attribute', default='location', visible=False)
 
-    def __init__(self):
-        super().__init__()
-        self._geolocator = None
-
-    def configure(self, context):
-        super().configure(context)
-        self._geolocator = Nominatim()
-
-    def process_signals(self, signals, input_id='default'):
-        for signal in signals:
-            self._add_location_to_signal(signal)
-        self.notify_signals(signals)
-
-    def _add_location_to_signal(self, signal):
-        """ Adds location data to the signal """
+    def _get_query_from_signal(self, signal):
         try:
             latitude = float(self.location.latitude(signal))
             longitude = float(self.location.longitude(signal))
-            latlong = (latitude, longitude)
+            return (latitude, longitude)
         except:
             self._logger.exception("Unable to evaluate location property")
-            setattr(signal, self.output_prop, None)
-            return
-        try:
-            self._logger.debug(
-                "Geocode location \"{}\"".format(latlong))
-            location = self._geolocator.reverse(latlong)
-            self._logger.debug(
-                "Geocode result for location \"{}\": {}".format(latlong, location))
-            if not location:
-                self._logger.warning(
-                    "No geocode loaction for location: {}".format(latlong))
-                return
-            location_dict = AttributeDict({
-                'address': location.address,
-                'altitude': location.altitude,
-                'latitude': location.latitude,
-                'longitude': location.longitude,
-                'raw': location.raw
-            })
-            setattr(signal, self.output_prop, location_dict)
-        except:
-            self._logger.exception(
-                "Unable to geocode location from location: {}".format(latlong))
-        finally:
-            if not getattr(signal, self.output_prop, None):
-                setattr(signal, self.output_prop, None)
+
+    def _get_location(self, query):
+        return self._geolocator.reverse(query)
